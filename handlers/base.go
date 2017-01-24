dadfb34d0beddc9d1556166e29f0b49f82e69dd5
Juan F. Giménez Silva <juanfgs@gmail.com>
2017-01-24 10:21:31 -0300

    added data and funcs

--- handlers/base.go
+++ handlers/base.go
@@ -12,9 +12,20 @@ import (
 type BaseHandler struct {
 	templates []string
 	tpl *template.Template  /* Compiled template */
+	Data map[string]interface{}
+	Funcs map[string]interface{}
 }
 
+func (self *BaseHandler) AddFunc(name string, f interface{}){
+	self.initializeFuncs()
+	self.Funcs[name] = f
+}
 
+func (self *BaseHandler) initializeFuncs(){
+	if len(self.Funcs) == 0 {
+		self.Funcs = make(map[string]interface{})
+	}
+}
 
 func (self *BaseHandler)  AddTemplate(templateName string){
 	self.templates = append(self.templates, config.Values.AppPath + "views/" + templateName)
@@ -22,8 +33,8 @@ func (self *BaseHandler)  AddTemplate(templateName string){
 
 func (self BaseHandler) Render(w http.ResponseWriter, data interface{}) {
 	var err error
-
-	self.tpl, err = template.New("layout").ParseFiles(self.templates...)
+	self.initializeFuncs()
+	self.tpl, err = template.New("layout").Funcs(self.Funcs).ParseFiles(self.templates...)
 	if err != nil{
 		panic(err)
 	}
