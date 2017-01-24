b3da3b746b54c18cfa8503630d54e9ccd0a121d5
Juan F. Giménez Silva <juanfgs@gmail.com>
2017-01-24 10:21:39 -0300

    added migration support

--- models/model.go
+++ models/model.go
@@ -1,6 +1,8 @@
 package models
 
 import(
+	"github.com/mattes/migrate/migrate"
+	_ "github.com/mattes/migrate/driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"database/sql"
 	"log"
@@ -8,6 +10,8 @@ import(
 )
 
 var DB *sql.DB
+
+var driverString string = config.Values.Database.Username + ":" + config.Values.Database.Password + "@tcp("+ config.Values.Database.Host +":"+ config.Values.Database.Port +")/"+ config.Values.Database.Name + "?parseTime=true"
 
 type Model struct {
 	Written bool
@@ -18,12 +22,11 @@ type Model struct {
 
 func init() {
 	var dbError error
-	DB, dbError = sql.Open(config.Values.Database.Driver, config.Values.Database.Username + ":" + config.Values.Database.Password + "@tcp("+ config.Values.Database.Host +":"+ config.Values.Database.Port +")/"+ config.Values.Database.Name + "?parseTime=true")
-	
+	DB, dbError = sql.Open(config.Values.Database.Driver, driverString)
 	if dbError != nil {
 		log.Fatal("Error : ", dbError)
 	}
-} 
+}
 
 func (this Model) FindById( id int64, fields ...interface{})  {
 	err:= DB.QueryRow("SELECT * FROM `"+ this.tableName +"` WHERE `"+this.indexName+"` = ?", id ).Scan(fields...)
@@ -34,12 +37,20 @@ func (this Model) FindById( id int64, fields ...interface{})  {
 }
 
 func (this Model) FindByField( field string, value string, fields ...interface{})  {
-	
 	err:= DB.QueryRow("SELECT * FROM `"+ this.tableName +"` WHERE `"+ field +"` = ?", value ).Scan(fields...)
 	if err != nil {
 		log.Println(err)
 	}
 
+}
+
+func (this Model) All() *sql.Rows{
+	rows, err:= DB.Query("SELECT * FROM `"+ this.tableName +"`" )
+	if err != nil {
+		log.Println(err)
+	}
+
+	return rows
 }
 
 
@@ -51,3 +62,17 @@ func (this *Model) SetTable(table string){
 	this.tableName = table
 }
 
+func MigrationUp(){
+	errors, ok := migrate.UpSync( config.Values.Database.Driver +"://"+ driverString, "../migrations/")
+	if !ok {
+		panic(errors)
+	}
+}
+
+func MigrationDown(){
+	errors, ok := migrate.DownSync(config.Values.Database.Driver + "://" + driverString, "../migrations/")
+	if !ok {
+		log.Println(errors)
+		panic(errors)
+	}
+}
