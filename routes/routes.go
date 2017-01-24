1690bb1608f9559662d0b56d71900870cdabe1f4
Juan F. Giménez Silva <juanfgs@gmail.com>
2017-01-24 10:21:48 -0300

    added admin routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import(
 
+	"github.com/juanfgs/cv/handlers/admin"
 	"github.com/juanfgs/cv/handlers"
 	"net/http"
 )
@@ -15,7 +16,7 @@ type Route struct {
 func Get() []Route{
 	return []Route{
 		{ "/", handlers.FormHandler{}.Index },
-		{ "/tictactoe", handlers.FormHandler{}.Index },
-		{ "/create", handlers.FormHandler{}.Post },
+		{ "/admin", admin.AdminHandler{}.Index },
+		{ "/admin/sections", admin.SectionsHandler{}.Index },
 	}
 }
