4844346766d0697f4fa1b93c8c2b0d13aaff942a
Juan F. Giménez Silva <juanfgs@gmail.com>
2017-02-03 19:46:24 -0300

    added CRUD routes

--- routes/routes.go
+++ routes/routes.go
@@ -10,13 +10,24 @@ import(
 type Route struct {
 	Destination string
 	Handler func( w http.ResponseWriter, r *http.Request) ()
+	Method string
 }
 
 
 func Get() []Route{
 	return []Route{
-		{ "/", handlers.FormHandler{}.Index },
-		{ "/admin", admin.AdminHandler{}.Index },
-		{ "/admin/sections", admin.SectionsHandler{}.Index },
+		{ "/", handlers.FormHandler{}.Index, "GET" },
+		{ "/admin", admin.AdminHandler{}.Index , "GET"},
+		{ "/admin/sections", admin.SectionsHandler{}.Index, "GET" },
+		{ "/admin/sections/create", admin.SectionsHandler{}.CreateForm, "GET" },
+		{ "/admin/sections/create", admin.SectionsHandler{}.Create, "POST"},
+		{ "/admin/sections/edit/{id}", admin.SectionsHandler{}.EditForm, "GET"},
+		{ "/admin/sections/edit/{id}", admin.SectionsHandler{}.Edit, "POST"},
+		{ "/admin/sections/delete/{id}", admin.SectionsHandler{}.Delete, "GET"},
+		{ "/admin/items", admin.ItemsHandler{}.Index, "GET" },
+		{ "/admin/items/create", admin.ItemsHandler{}.CreateForm, "GET" },
+		{ "/admin/items/create", admin.ItemsHandler{}.Create, "POST"},
+		{ "/admin/items/create", admin.ItemsHandler{}.Create, "POST"},
+		{ "/data.json", handlers.JsonHandler{}.Index, "GET"},
 	}
 }
